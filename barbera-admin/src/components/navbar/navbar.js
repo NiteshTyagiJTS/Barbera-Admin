d5bccb7d38e694d20b6c7c6f83dab8aba0423c8e
NiteshTyagiJTS <nitesh.tyagi@jethitech.com>
2023-05-31 09:03:16 +0530

    commit - 5

--- barbera-admin/src/components/navbar/navbar.js
+++ barbera-admin/src/components/navbar/navbar.js
@@ -1,10 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import Sidebar from '../sidebar/sidebar';
 
 function Navbar({ onLogout, userId }) {
+  const [isShown, setIsShown] = useState(false);
+  const openSidebar = () => {
+    setIsShown(current => !current);
+  };
+
+  const hideSidebar = (event) => {
+    // if (window.innerWidth < 640 && isShown && event.touches[0].clientX > window.innerWidth / 2) {
+    //   setIsShown(false);
+    // }
+    if (isShown && event.touches[0].clientX > window.innerWidth / 2) {
+      setIsShown(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('touchstart', hideSidebar);
+    return () => {
+      window.removeEventListener('touchstart', hideSidebar);
+    };
+  }, []);
+
   return (
-    <div className="flex flex-row justify-between items-center px-24 absolute w-full h-16 left-0 top-5 bg-zinc-900 shadow-md">
-      <p>Navbar</p>
+    <div>
+      <div className="flex flex-row justify-between items-center px-24 absolute w-full h-16 left-0 top-5 bg-zinc-900 shadow-md">
+        <button
+          className="text-red-100 absolute left-5 text-yellow-600 left-1/4 absolute text-2xl"
+          onClick={openSidebar}
+        >
+          &#9776;
+        </button>
+        <p className="text-yellow-500 left-1/4 absolute text-lg sm:text-3xl">Hello Smaran!</p>
+        <p className="text-yellow-500 right-10 sm:right-20 absolute text-sm sm:text-md">Smaran U.</p>
+      </div>
+
+      {isShown && <Sidebar />}
+      <div className="hidden sm:block">
+        <Sidebar />
+      </div>
     </div>
   );
 }
--- barbera-admin/src/pages/login/login.js
+++ barbera-admin/src/pages/login/login.js
@@ -19,6 +19,7 @@ const Login = ({ onLogin }) => {
 
   return (
     <div className="relative bg-stone-900 min-h-screen">
+      <div className="sm:block hidden">
       <div className="absolute inset-y-0 left-20 w-30">
         <h2 className="relative top-1/4 left-1/2 font-jost font-normal text-base leading-6 text-yellow-500 py-2 px-4">Welcome to Barbera</h2>
         <br></br>
@@ -43,8 +44,34 @@ const Login = ({ onLogin }) => {
           </button>
         </div>
       </div>
-      <div className="absolute inset-y-0 right-0 w-2/3">
-        <img src={LoginPageImage} alt="Login page" className="w-full h-full object-cover" />
+      </div>
+
+      <div className="sm:hidden">
+      <div className="absolute inset-0 flex flex-col justify-center items-center p-6 sm:p-10 md:p-16 lg:p-20 xl:p-24">
+        <h2 className="font-jost font-normal text-2xl text-yellow-500 mb-4">Welcome to Barbera</h2>
+        <div className="mb-4 w-full">
+          <label htmlFor="otp" className="text-white font-medium mb-2 block">
+            Login with Mobile
+          </label>
+          <input
+            type="text"
+            id="otp"
+            className="w-full bg-white text-gray-800 py-2 px-4 rounded-md shadow-sm"
+          />
+        </div>
+        <div className="w-full">
+          <button
+            className="bg-yellow-500 text-white py-2 px-4 rounded-md w-full"
+            onClick={handleRedirect}
+            disabled={loading}
+          >
+            {loading ? 'Logging in...' : 'Send OTP'}
+          </button>
+        </div>
+      </div>
+      </div>
+      <div className="absolute inset-0 hidden sm:block left-1/2 justify-end">
+        <img src={LoginPageImage} alt="Login page" className="h-full object-cover" />
       </div>
     </div>
   );
