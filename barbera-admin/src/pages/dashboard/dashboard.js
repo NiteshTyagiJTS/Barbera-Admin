e8c85fd83f1c6533197b21c07f9a1383eaed2f17
NiteshTyagiJTS <nitesh.tyagi@jethitech.com>
2023-05-31 09:03:07 +0530

    commit - 4

--- barbera-admin/src/pages/dashboard/dashboard.js
+++ barbera-admin/src/pages/dashboard/dashboard.js
@@ -1,9 +1,23 @@
 import React, { useState } from 'react';
-
+import Bookings from './Total-booking.svg';
+import Earning from './Total-earnings.svg';
+import DownArrow from './DownArrow.svg';
 function Dashboard () {
     return (
-        <div>
-            <p>dashboard</p>
+        <div className="flex flex-row align-center">
+            <div className="absolute">
+                <div className="flex flex-row">
+                <p className="font-jost font-bold text-20 leading-29 text-yellow-500">April Activity</p>
+                <img src={DownArrow} className="mx-2 my-3"/>
+                </div>
+            </div>
+            <div className="bg-yellow-500 container h-full w-1/4 mr-10 mt-16 rounded-3xl">
+                <img src={Bookings} className="mx-auto my-5 " />
+            </div>
+            <div className="bg-yellow-500 container h-90 w-1/4 mt-16 rounded-3xl">
+                <img src={Earning} className="mx-auto my-5" />
+                
+            </div>
         </div>
     );
 }
